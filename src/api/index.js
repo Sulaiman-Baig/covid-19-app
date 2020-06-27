03352301d021b4308f561f3b98829001ae065273
sulaimanbaig <engsulaimanbaig@gmailcom>
2020-06-27 19:31:57 +0500

    Destructuring Data from API

--- src/api/index.js
+++ src/api/index.js
@@ -4,9 +4,40 @@ const url = 'https://covid19.mathdro.id/api';
 
 export const fetchData = async () => {
     try {
-        const response = await axios.get(url);
-        return response;
+        const { data: { confirmed, recovered, deths, lastUpdate } } = await axios.get(url);
+        return { confirmed, recovered, deths, lastUpdate };
+
     } catch (err) {
 
     }
 }
+
+// const {data} = await axios.get(url);
+
+//         const modifiedData = {
+//             confirmed: data.confirmed,
+//             countryDetails: data.recoverd,
+//             dailyTimeSeries: data.deths,
+//             recovered: data.lastUpdate
+//         }
+//         return modifiedData;
+
+// const { data: { confirmed, recovered, deths, lastUpdate } } = await axios.get(url);
+
+//         const modifiedData = {
+//             confirmed: confirmed,
+//             countryDetails: recovered,
+//             dailyTimeSeries: deths,
+//             recovered: lastUpdate
+//         }
+//         return modifiedData;
+
+// const { data: { confirmed, recovered, deths, lastUpdate } } = await axios.get(url);
+
+//         const modifiedData = {
+//             confirmed,
+//             deths,
+//             lastUpdate,
+//             recovered
+//         }
+//         return modifiedData;
