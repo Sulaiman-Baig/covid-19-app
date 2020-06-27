53e8eb3f797d39a864c734efdb0f22da4da20b20
sulaimanbaig <engsulaimanbaig@gmailcom>
2020-06-27 19:38:51 +0500

    death typo fixed

--- src/api/index.js
+++ src/api/index.js
@@ -4,8 +4,8 @@ const url = 'https://covid19.mathdro.id/api';
 
 export const fetchData = async () => {
     try {
-        const { data: { confirmed, recovered, deths, lastUpdate } } = await axios.get(url);
-        return { confirmed, recovered, deths, lastUpdate };
+        const { data: { confirmed, recovered, deaths, lastUpdate } } = await axios.get(url);
+        return { confirmed, recovered, deaths, lastUpdate };
 
     } catch (err) {
 
